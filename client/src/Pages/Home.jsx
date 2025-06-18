7dbe876943d2ed43d96e2dacf8adb22116aa5e8d
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2025-06-19 00:11:51 +0530

    Created Newsletter section Successfully

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -3,6 +3,7 @@ import Hero from '../Components/Hero'
 import FeaturedDestinations from '../Components/FeaturedDestinations'
 import ExclusiveOffers from '../Components/ExclusiveOffers'
 import Testimonial from '../Components/Testimonial'
+import NewsLetter from '../Components/NewsLetter'
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <FeaturedDestinations />
             <ExclusiveOffers />
             <Testimonial />
+            <NewsLetter />
         </>
     )
 }
