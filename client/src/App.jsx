0f1dc2f6acb5d4960656f6e9567528d90fce5700
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2025-06-18 16:01:44 +0530

    Created Hero section Successfully

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './Pages/Home';
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div >
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,10 +2,18 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-createRoot(document.getElementById('root')).render(
+import { ClerkProvider } from '@clerk/clerk-react'
+// Import your Publishable Key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key')
+}
+createRoot(document.getElementById('root')).render(
+  <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ClerkProvider>
 
 )
