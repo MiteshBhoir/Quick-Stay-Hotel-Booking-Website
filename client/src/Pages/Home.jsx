cbddea9a97845da42942aa4f490e1c34f2be2a1d
Mitesh Bhoir <miteshbhoirofficial@gmail.com>
2025-06-19 00:01:41 +0530

    Created Exclusive offers and Testimonials  section Successfully

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../Components/Hero'
 import FeaturedDestinations from '../Components/FeaturedDestinations'
+import ExclusiveOffers from '../Components/ExclusiveOffers'
+import Testimonial from '../Components/Testimonial'
 
 const Home = () => {
     return (
         <>
             <Hero />
-            <FeaturedDestinations/>
+            <FeaturedDestinations />
+            <ExclusiveOffers />
+            <Testimonial />
         </>
     )
 }
